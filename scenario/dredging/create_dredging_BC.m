52814ee633f9afe5c3aa0e2ff2dbcf97c13cce16
Matt Hipsey <matt.hipsey@uwa.edu.au>
2025-10-04 15:39:27 +0800

    add dredging scenario scripts

---
+++ scenario/dredging/create_dredging_BC.m
@@ -0,0 +1,49 @@
+
+clear; close all;
+
+datearray=datenum(2022,1,1):1/24:datenum(2023,1,1);
+
+t1=datenum(2022,1,15);
+t2=datenum(2022,2,15);
+
+ls=[115.7085 -32.1537];
+le=[115.6966 -32.0486];
+
+outfile='W:\csiem\Model\TFV\csiem_model_tfvaed_1.1\bc_repo\9_operations\dredging\mimic_dredge.csv';
+
+header='Date,lon,lat,Salt,Temp,TSS,DO,AMM,NIT,FRP,FRPADS,POC,DOC,PON,DON,POP,DOP,GRN,BGA,CRYPT,MDIAT,DINO,ZEROS,ONES';
+fileID = fopen(outfile,'w');
+fprintf(fileID,'%s\n',header);
+
+for i=1:length(datearray)
+    if mod(i,1000)==0
+        disp(datestr(t0,'dd/mm/yyyy HH:MM:SS'));
+    end
+    t0=datearray(i);
+    datev=datevec(t0);
+    datevh=datev(4);
+
+    if t0<=t1
+        lon=ls(1);
+        lat=ls(2);
+    elseif t0>=t2
+        lon=le(1);
+        lat=le(2);
+    else
+        lon=ls(1)+(le(1)-ls(1))/(t2-t1)*(t0-t1);
+        lat=ls(2)+(le(2)-ls(2))/(t2-t1)*(t0-t1);
+    end
+
+    fprintf(fileID,'%s,',datestr(t0,'dd/mm/yyyy HH:MM:SS'));
+    fprintf(fileID,'%4.4f,',lon);
+    fprintf(fileID,'%4.4f,',lat);
+
+    if (t0>t1 && t0<t2 && datevh>8 && datevh<16)
+        fprintf(fileID,'%s\n','0,0,1000,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1');
+    else
+        fprintf(fileID,'%s\n','0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1');
+    end
+
+end
+
+fclose(fileID);
